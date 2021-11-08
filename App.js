3c281237a5d2daa440ea4459c4ac14dff64f3d91
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-11-08 12:29:30 +0500

    create a channel

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useEffect } from "react";
+import { View, Text, Button } from "react-native";
+import PushNotification from "react-native-push-notification";
 
 const App = () => {
+
+  useEffect(() => {
+    createChannel()
+  }, [])
+
+  const createChannel = () => {
+    PushNotification.createChannel({
+      channelId: 'test-channel',
+      channelName: 'Test Channel'
+    })
+  }
+
   return(
     <View>
       <Text style={{fontSize:20, alignSelf:"center", marginVertical:10}}>Push Notification</Text>
+      <Button title="Show Notification" color="lightgreen" onPress={ } />
     </View>
   )
 }
