855560744100c982f405e03480b265cfb4980b8e
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-11-08 11:54:46 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App = () => {
+  return(
+    <View>
+      <Text style={{fontSize:20, alignSelf:"center", marginVertical:10}}>Push Notification</Text>
+    </View>
+  )
+}
+
+export default App;
