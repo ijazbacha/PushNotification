93a526ecdee86e0feada79f9bdc5cdb0a9557755
Ijaz Bacha <76259605+bacha-11@users.noreply.github.com>
2021-11-08 12:58:38 +0500

    localNotification

--- App.js
+++ App.js
@@ -15,10 +15,18 @@ const App = () => {
     })
   }
 
+  const handleNotification = () => {
+    PushNotification.localNotification({
+      channelId:"test-channel",
+      title: "Title of the message !!:D",
+      message: "try to show notification"
+    })
+  }
+
   return(
-    <View>
+    <View style={{ padding:10}}>
       <Text style={{fontSize:20, alignSelf:"center", marginVertical:10}}>Push Notification</Text>
-      <Button title="Show Notification" color="lightgreen" onPress={ } />
+      <Button title="Show Notification" color="green" onPress={() => handleNotification()} />
     </View>
   )
 }
